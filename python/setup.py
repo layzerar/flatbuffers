c9622121831138cafeaf303ea92535289ff698be
Melvi Ts <layzerar@gmail.com>
2015-10-30 17:15:26 +0800

    Python: Bump version to 1.2.0a

--- python/setup.py
+++ python/setup.py
@@ -85,7 +85,7 @@ def status_msgs(*msgs):
 def run_setup(with_cext):
     setup_kwds = dict(
         name='flatbuffers',
-        version='2015.05.14.0',
+        version='1.2.0a',
         license='Apache 2.0',
         author='FlatBuffers Contributors',
         author_email='me@rwinslow.com',
